8c2c9c948c83f32ee1b2316628440bd64d1a7259
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2022-08-15 16:04:22 +0700

    Add toggleable dropdown menu

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ const openBtn = document.querySelector('.open-menu')
 const closeBtn = document.querySelector('.close-menu')
 const overlay = document.querySelector('.overlay')
 
-// Event Listener
-openBtn.addEventListener('click', () => {
+const dropdowns = document.querySelectorAll('.dropdown')
+const dropdownBtns = document.querySelectorAll('.dropdown__btn')
+
+// Functions
+function openSidebar() {
   sidebar.dataset.state = 'open'
   overlay.dataset.state = 'open'
-})
+}
 
-closeBtn.addEventListener('click', () => {
+function closeSidebar() {
   sidebar.dataset.state = 'close'
   overlay.dataset.state = 'close'
-})
+}
 
-overlay.addEventListener('click', () => {
-  sidebar.dataset.state = 'close'
-  overlay.dataset.state = 'close'
+function toggleDropdown(dropdown) {
+  dropdown.dataset.state =
+    dropdown.dataset.state === 'expanded' ? 'collapsed' : 'expanded'
+}
+
+// Event Listener
+openBtn.addEventListener('click', openSidebar)
+
+closeBtn.addEventListener('click', closeSidebar)
+
+overlay.addEventListener('click', closeSidebar)
+
+dropdowns.forEach((dropdown, i) => {
+  dropdownBtns[i].addEventListener('click', () => {
+    toggleDropdown(dropdown)
+  })
 })
